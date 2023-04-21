474b631c89b39a232f19873993f454d20f64fe9d
marioprofertil <131455652+marioprofertil@users.noreply.github.com>
2023-04-21 08:35:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-download-file-base64-rsdqx8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-download-file-base64-rsdqx8)
---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Profertil';
+
+  onClick() {
+    console.log('23333');
+    this.showPdf();
+  }
+  showPdf() {
+    const linkSource =
+      'data:application/octet-stream;base64,' + 'Ingrese el codigo aqui';
+    const downloadLink = document.createElement('a');
+    const fileName = 'sample.docx';
+
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    downloadLink.click();
+  }
+}
